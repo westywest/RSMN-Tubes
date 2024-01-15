6f0633858db8d5047e19f0fd8a0b260b32344c94
westywest <dianmaharanidi17@gmail.com>
2024-01-15 10:23:45 +0700

    commit 5

--- rsmn/admin/dashboard.php
+++ rsmn/admin/dashboard.php
@@ -45,7 +45,7 @@
                         </a>
                     </li>
                     <li>
-                        <a href="/RSMN/rsmn/pasien/pendaftaran.php" class="nav-link link-dark">
+                        <a href="#" class="nav-link link-dark">
                             <svg class="bi pe-none me-2" width="16" height="16"><use xlink:href="#speedometer2"></use></svg>
                             Dokter
                         </a>
